4818775c5e3e9f07a4d6d108f351d9f4366031bc
Anjal Niraula <anjalniraula334@gmail.com>
2024-12-09 22:55:30 +0545

    DataTypes in JS

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hello World") // we are using node, not browser 
+
+//  code readabilty should be high
+
+console.log(3+3);
+console.log("Anjal Niraula");
+
+let name = "Anjal Noraula"
+let age = 17
+let isLoggedIn = false
+let state = null
+
+console.table([name, age, isLoggedIn, state]);
+
+// Int
+// BigInt
+// String
+// Boolean
+// null  -- standalone Value
+// undefined
+// symbol -- unique
+
+
+
+// object
+
+
+console.log(typeof null);
+console.log(typeof udefined);
+
