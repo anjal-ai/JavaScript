61df5f97da6b3ee15f9bfa53d51a603f146424cd
Anjal Niraula <anjalniraula334@gmail.com>
2024-12-10 17:36:56 +0545

    Conversion Operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "Anjal"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*
+"33" -- 33
+"33abc" -- NaN
+true -- 1; false -- 0
+*/
+
+let isLoggedIn = 1
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn);
+
+// 1 -- true; 0 -- false
+// "" -- false
+// "anjal" -- true
+
+
+let someNumber = 10
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
