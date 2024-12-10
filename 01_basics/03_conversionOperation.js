b97a0a36321bf06da8800e8a6f57d76fd430f5c0
Anjal Niraula <anjalniraula334@gmail.com>
2024-12-10 18:48:36 +0545

    Conversion Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,7 +26,60 @@ let booleanLoggedIn = Boolean(isLoggedIn)
 let someNumber = 10
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//  --------------------------  Operation  ------------------------
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%3);
+
+
+let str1 = "Hello"
+let str2 = " Anjal"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 2 * 5 % 4);       // --> not practical use
+
+
+// tricky conversion
+
+// console.log(+true);
+// console.log(+"");
+
+
+// not practical approach
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+// console.table([num1, num2, num3])
+
+let counter = 1
+let counter1 = 1
+// counter++
+// console.log(counter);
+
+
+// prefix and postfix in js
+console.log(counter++);
+console.log(++counter1);
 
 
