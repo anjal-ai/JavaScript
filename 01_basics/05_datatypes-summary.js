d7faf4abe5fa6e0fcff1b4abe235a427411eb258
Anjal Niraula <anjalniraula334@gmail.com>
2024-12-16 17:40:01 +0545

    datatype summary

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,38 @@
+//  Primitive Data Type
+// 7 types :
+// String, Number , Boolean, Null, Undefined, Symbol, BigInt
+
+// const score:number = 100     -- typescript
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outSideTemp = null
+// const userEmail = undefined
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id == anotherId);
+
+const bigNumber = 7382859898598n 
+
+// ----------------------------------------------------------------------------------------
+
+
+// Reference (Non-Primitive)
+// Array, Object, Function
+
+const heros = ["Anjal", "Aayush", "Rohan", "Aashish"]
+
+let myObj = {
+    name : "Anjal",
+    age : 17
+}
+
+const myFunction = function(){
+    console.log("Hello World!");
+}
+
+console.log(typeof outSideTemp);
