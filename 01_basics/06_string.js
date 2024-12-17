b8cf32232723251cd0e1a936e342c627fed35541
Anjal Niraula <anjalniraula334@gmail.com>
2024-12-17 09:09:17 +0545

    String

---
+++ 01_basics/06_string.js
@@ -0,0 +1,38 @@
+const name = "Anjal"
+
+const repoCount = 50
+
+// console.log(name + repoCount);
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Anjal Niraula')
+
+console.log(gameName);
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.toLocaleLowerCase());
+// console.log(gameName.charAt(0));
+// console.log(gameName.indexOf('a'));
+
+const newString = gameName.substring(0, 5)
+// console.log(newString);
+
+const anotherString = gameName.slice(-10, -6)
+// console.log(anotherString);
+
+const newString1 = "    Anjal    "
+// console.log(newString1);
+// console.log(newString1.trim());
+
+const url = "https://anjal.com/anjal%20niraula"
+
+console.log(url.replace('%20', '-'))
+
+
+
+
+
